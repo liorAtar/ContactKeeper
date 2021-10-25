c13e9b43164765cdf0ff25e972568e979c58c5a1
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2021-10-25 13:01:11 +0300

    added contact model and get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth')
+const { check, validationResult} = require('express-validator/check')
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route     GET api/contacts
 // @desc      Get all users contacts
 // @access    Private
-router.get('/', (req, res) => {
-    res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+    try {
+        // Get the contacts from the Database when the date is the last parameter
+        const contacts = await Contact.find({ user: req.user.id}).sort({
+            date: -1
+        });
+        res.json(contacts);
+    } catch (err) {
+        console.log(err.message);
+
+        // Send Server Error
+        res.status(500).json('Server Error');
+    }
 });
 
 // @route     POST api/contacts
